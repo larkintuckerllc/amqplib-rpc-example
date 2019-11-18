c1d5c4c3f644e7518d0ce0d54e80cea3c5556275
John Tucker <jtucker@appirio.com>
2019-11-18 13:10:51 -0500

    amqp

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,19 @@ import express from 'express';
 import { connect } from 'amqplib';
 
 const { CLOUDAMQP_URL, PORT } = process.env;
+const QUEUE = 'tasks';
 
 const execute = async (): Promise<void> => {
   const conn = await connect(CLOUDAMQP_URL);
+  const ch = await conn.createChannel();
+  ch.assertQueue(QUEUE);
   const app = express();
   app.use(cors());
   app.get('/', (req, res) => res.send({ hello: 'world' }));
+  app.get('/upload', (req, res) => {
+    ch.sendToQueue(QUEUE, Buffer.from('hello world'));
+    res.send({ hello: 'upload' });
+  });
   // eslint-disable-next-line
   app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
 };
