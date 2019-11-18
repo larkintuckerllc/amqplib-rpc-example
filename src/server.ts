e4e3b920d79b28933e496d24d1ac8d4b17d48916
John Tucker <jtucker@appirio.com>
2019-11-18 12:49:20 -0500

    basic rabbit

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import cors from 'cors';
 import express from 'express';
+import { connect } from 'amqplib';
 
-const PORT = process.env.PORT || 3000;
+const { CLOUDAMQP_URL, PORT } = process.env;
 
-const app = express();
-app.use(cors());
-app.get('/', (req, res) => res.send({ hello: 'world' }));
-// eslint-disable-next-line
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+const execute = async (): Promise<void> => {
+  const conn = await connect(CLOUDAMQP_URL);
+  const app = express();
+  app.use(cors());
+  app.get('/', (req, res) => res.send({ hello: 'world' }));
+  // eslint-disable-next-line
+  app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+};
+execute();
