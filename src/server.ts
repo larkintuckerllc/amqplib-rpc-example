a70e7914c20fc76bec97cc3c2e8b9ae49ea45359
John Tucker <jtucker@appirio.com>
2019-11-18 14:27:22 -0500

    WTF

--- src/server.ts
+++ src/server.ts
@@ -8,12 +8,12 @@ const QUEUE = 'tasks';
 const execute = async (): Promise<void> => {
   const conn = await connect(CLOUDAMQP_URL);
   const ch = await conn.createChannel();
-  ch.assertQueue(QUEUE);
+  await ch.assertQueue(QUEUE);
   const app = express();
   app.use(cors());
   app.get('/', (req, res) => res.send({ hello: 'world' }));
   app.get('/upload', (req, res) => {
-    ch.sendToQueue(QUEUE, Buffer.from('hello world'));
+    ch.sendToQueue(QUEUE, Buffer.from('hello'));
     res.send({ hello: 'upload' });
   });
   // eslint-disable-next-line
--- src/worker.ts
+++ src/worker.ts
@@ -3,14 +3,22 @@ import { connect, ConsumeMessage } from 'amqplib';
 const { CLOUDAMQP_URL } = process.env;
 const QUEUE = 'tasks';
 
+const delay = (): Promise<void> =>
+  new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, 5000);
+  });
+
 const execute = async (): Promise<void> => {
   const conn = await connect(CLOUDAMQP_URL);
   const ch = await conn.createChannel();
   ch.assertQueue(QUEUE);
-  const handleConsume = (msg: ConsumeMessage): void => {
+  const handleConsume = async (msg: ConsumeMessage): Promise<void> => {
     if (msg === null) {
       return;
     }
+    await delay();
     console.log(msg.content.toString());
     ch.ack(msg);
   };
