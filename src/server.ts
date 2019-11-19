47540d6fd866b4863c5da679dc49a7978dcdcf42
John Tucker <jtucker@appirio.com>
2019-11-18 19:43:16 -0500

    unique client queues

--- src/server.ts
+++ src/server.ts
@@ -4,26 +4,28 @@ import { connect, ConsumeMessage } from 'amqplib';
 
 const { CLOUDAMQP_URL, PORT } = process.env;
 const QUEUE_TASKS = 'tasks';
-const QUEUE_RESULTS = 'results';
 
 const execute = async (): Promise<void> => {
   const conn = await connect(CLOUDAMQP_URL);
   const ch = await conn.createChannel();
   await ch.assertQueue(QUEUE_TASKS);
-  await ch.assertQueue(QUEUE_RESULTS);
+  const { queue } = await ch.assertQueue('', {
+    exclusive: true,
+  });
   const handleConsume = async (msg: ConsumeMessage): Promise<void> => {
     if (msg === null) {
       return;
     }
-    console.log(msg.content.toString());
+    const { content } = msg;
+    console.log(content.toString());
     ch.ack(msg);
   };
-  ch.consume(QUEUE_RESULTS, handleConsume);
+  ch.consume(queue, handleConsume);
   const app = express();
   app.use(cors());
   app.get('/', (req, res) => res.send({ hello: 'world' }));
   app.get('/upload', (req, res) => {
-    ch.sendToQueue(QUEUE_TASKS, Buffer.from('hello'));
+    ch.sendToQueue(QUEUE_TASKS, Buffer.from('hello'), { replyTo: queue });
     res.send({ hello: 'upload' });
   });
   // eslint-disable-next-line
--- src/worker.ts
+++ src/worker.ts
@@ -2,19 +2,21 @@ import { connect, ConsumeMessage } from 'amqplib';
 
 const { CLOUDAMQP_URL } = process.env;
 const QUEUE_TASKS = 'tasks';
-const QUEUE_RESULTS = 'results';
 
 const execute = async (): Promise<void> => {
   const conn = await connect(CLOUDAMQP_URL);
   const ch = await conn.createChannel();
   ch.assertQueue(QUEUE_TASKS);
-  ch.assertQueue(QUEUE_RESULTS);
   const handleConsume = async (msg: ConsumeMessage): Promise<void> => {
     if (msg === null) {
       return;
     }
-    console.log(msg.content.toString());
-    ch.sendToQueue(QUEUE_RESULTS, Buffer.from('world'));
+    const {
+      content,
+      properties: { replyTo },
+    } = msg;
+    console.log(content.toString());
+    ch.sendToQueue(replyTo, Buffer.from('world'));
     ch.ack(msg);
   };
   ch.consume(QUEUE_TASKS, handleConsume);
